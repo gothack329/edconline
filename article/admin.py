58499c0c76f269d0bb6f3b20a4f18e594ef9391d
gothack329 <gothack.329@gmail.com>
2018-02-24 12:11:14 +0800

    add form

--- article/admin.py
+++ article/admin.py
@@ -3,6 +3,15 @@ from article.models import *
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id','title','author','publish_date','visible')
-
+    list_display = ('id','section','title','author','author_id','publish_time','visible')
 admin.site.register(Article,ArticleAdmin)
+
+
+class SectionAdmin(admin.ModelAdmin):
+    list_display=('id','name')
+admin.site.register(Section,SectionAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=('id','article','user','mail','ip','comment_time','comment')
+admin.site.register(Comment,CommentAdmin)
--- article/models.py
+++ article/models.py
@@ -2,32 +2,74 @@
 from django.db import models
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
+from django.forms import ModelForm,Textarea
 #from django.conf import settings
 #from tagging.fields import TagField
 #from tagging.models import Tag
 
 # Create your models here.
 
+class Section(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table ="Section"
+
+
 class Article(models.Model):
     id=models.AutoField(primary_key=True)
-    #catalog=models.ForeignKey(Catalogs)
-    author = models.CharField(max_length=128,default='小志')
+    section=models.ForeignKey(Section,on_delete=models.CASCADE,default=0)
     author_id = models.ForeignKey(User,on_delete=models.CASCADE,default=0)
+    author = models.CharField(max_length=128,default='')
     #author_id = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    publish_date = models.DateTimeField(auto_now_add=True)
+    publish_time = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=1024)
     detail = HTMLField()
+    ip = models.GenericIPAddressField(blank=True,null=True)
     tag = models.CharField(max_length=128, blank=True, null=True)
-    visible = models.CharField(choices=((u'Y',u'是'),(u'N',u'否')),max_length=64,default='N')
+    visible = models.CharField(choices=(('Y','是'),('N','否')),max_length=64,default='N')
     readtime = models.IntegerField(default=0)
 
-    def set_tags(self, tags):
-        Tag.objects.update_tags(self, tags)
-    def get_tags(self, tags):
-        return Tag.objects.get_for_object(self)
-    def __unicode__(self):
+    #def set_tags(self, tags):
+    #    Tag.objects.update_tags(self, tags)
+    #def get_tags(self, tags):
+    #    return Tag.objects.get_for_object(self)
+    def __str__(self):
         return self.title
+
     class Meta:
-        db_table ="articles"
-        ordering = ['-publish_date']
+        db_table ="article"
+        ordering = ['-publish_time']
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title','author','section','tag','detail','ip']
+        widgets = {
+                    'detail':Textarea(),
+                }
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    article = models.ForeignKey(Article,on_delete=models.CASCADE,default=0)
+    user = models.CharField(max_length=128)
+    mail = models.CharField(max_length=1024)
+    comment_time = models.DateTimeField(auto_now_add=True)
+    comment = HTMLField()
+    ip = models.GenericIPAddressField()
+    clickcount = models.IntegerField(default=0)
+    refer = models.CharField(max_length=5096,default='none')
+    agent = models.CharField(max_length=5096,default='none')
+
+    def __str__(self):
+        return self.comment
+        #return '用户%s发表留言。' % {self.uname}
+    class Meta:
+        db_table ="comment"
+        ordering = ['-comment_time']
 
